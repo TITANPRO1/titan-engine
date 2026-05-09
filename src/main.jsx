10620cc531cae8e456e93d580e0f61895bb73969
TITANPRO1 <harshalighate1234@gmail.com>
2026-05-09 18:24:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TitanEngine from './TitanEngine'
+import TitanEngine from './TitanEngine.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
